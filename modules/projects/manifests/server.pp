d9e53cdf937a71d348980d26d7f3c0049490a051
Dongri Jin <dongriab@gmail.com>
2014-04-05 16:56:20 +0900

    fix

--- modules/projects/manifests/server.pp
+++ modules/projects/manifests/server.pp
@@ -2,8 +2,8 @@ class projects::server {
   include nginx
   include virtualbox
   include mongodb
-  include mysql # Disable
-  include redis # Disable
+  include mysql
+  include redis
   include heroku
   include imagemagick
 
